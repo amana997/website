fd364fa8f1f33478db2ea2a965bb9d16458135e3
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-12-27 17:23:42 +0530

    CodeChef

--- tableData/rangoli.sh
+++ tableData/rangoli.sh
@@ -2,6 +2,108 @@
 
 aws s3api put-object \
   --bucket asha-home-products-catalog \
-  --key products/rangoli-002.jpeg \
+  --key products/rangoli/rangoli-001.jpeg \
+  --body ~/Downloads/rangoli/rangoli-001.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=2500&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-001.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-002.jpeg \
   --body ~/Downloads/rangoli/rangoli-002.jpeg \
-  --tagging "name=rangoli&description=check&price=700&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli-002.jpeg"
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=1600&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-002.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-003.jpeg \
+  --body ~/Downloads/rangoli/rangoli-003.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=2800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-003.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-004.jpeg \
+  --body ~/Downloads/rangoli/rangoli-004.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=1300&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-004.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-005.jpeg \
+  --body ~/Downloads/rangoli/rangoli-005.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=2800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-005.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-006.jpeg \
+  --body ~/Downloads/rangoli/rangoli-006.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=1600&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-006.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-007.jpeg \
+  --body ~/Downloads/rangoli/rangoli-007.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=3000&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-007.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-008.jpeg \
+  --body ~/Downloads/rangoli/rangoli-008.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=2600&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-008.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-009.jpeg \
+  --body ~/Downloads/rangoli/rangoli-009.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-009.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-010.jpeg \
+  --body ~/Downloads/rangoli/rangoli-010.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=1300&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-010.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-011.jpeg \
+  --body ~/Downloads/rangoli/rangoli-011.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=1300&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-011.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-012.jpeg \
+  --body ~/Downloads/rangoli/rangoli-012.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=3500&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-012.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-013.jpeg \
+  --body ~/Downloads/rangoli/rangoli-013.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=1300&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-013.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-014.jpeg \
+  --body ~/Downloads/rangoli/rangoli-014.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=4000&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-014.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-015.jpeg \
+  --body ~/Downloads/rangoli/rangoli-015.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=3100&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-015.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-016.jpeg \
+  --body ~/Downloads/rangoli/rangoli-016.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=2600&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-016.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-017.jpeg \
+  --body ~/Downloads/rangoli/rangoli-017.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=1500&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-017.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/rangoli/rangoli-018.jpeg \
+  --body ~/Downloads/rangoli/rangoli-018.jpeg \
+  --tagging "name=rangoli&description=rangoli which can be arranged as per your choice&price=3000&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/rangoli/rangoli-018.jpeg"
