eec40e4d9daef0af4e50b92f0bc8d3326f9f63d1
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-12-27 18:22:40 +0530

    CodeChef

---
+++ tableData/thali.sh
@@ -0,0 +1,127 @@
+#commands to upload the data
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-001.jpeg \
+  --body ~/Downloads/thali/thali-001.jpeg \
+  --tagging "name=thali-01&description=Steel Thali&price=1400&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-001.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-002.jpeg \
+  --body ~/Downloads/thali/thali-002.jpeg \
+  --tagging "name=thali-02&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-002.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-003.jpeg \
+  --body ~/Downloads/thali/thali-003.jpeg \
+  --tagging "name=thali-03&description=Steel Thali&price=3100&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-003.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-004.jpeg \
+  --body ~/Downloads/thali/thali-004.jpeg \
+  --tagging "name=thali-04&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-004.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-005.jpeg \
+  --body ~/Downloads/thali/thali-005.jpeg \
+  --tagging "name=thali-05&description=Steel Thali&price=900&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-005.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-006.jpeg \
+  --body ~/Downloads/thali/thali-006.jpeg \
+  --tagging "name=thali-06&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-006.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-007.jpeg \
+  --body ~/Downloads/thali/thali-007.jpeg \
+  --tagging "name=thali-07&description=Small Wooden Thali&price=900&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-007.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-008.jpeg \
+  --body ~/Downloads/thali/thali-008.jpeg \
+  --tagging "name=thali-08&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-008.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-009.jpeg \
+  --body ~/Downloads/thali/thali-009.jpeg \
+  --tagging "name=thali-09&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-009.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-010.jpeg \
+  --body ~/Downloads/thali/thali-010.jpeg \
+  --tagging "name=thali-10&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-010.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-011.jpeg \
+  --body ~/Downloads/thali/thali-011.jpeg \
+  --tagging "name=thali-11&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-011.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-012.jpeg \
+  --body ~/Downloads/thali/thali-012.jpeg \
+  --tagging "name=thali-12&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-012.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-013.jpeg \
+  --body ~/Downloads/thali/thali-013.jpeg \
+  --tagging "name=thali-13&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-013.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-014.jpeg \
+  --body ~/Downloads/thali/thali-014.jpeg \
+  --tagging "name=thali-14&description=Small Wooden Thali&price=900&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-014.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-015.jpeg \
+  --body ~/Downloads/thali/thali-015.jpeg \
+  --tagging "name=thali-15&description=Small Wooden Thali&price=900&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-015.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-016.jpeg \
+  --body ~/Downloads/thali/thali-016.jpeg \
+  --tagging "name=thali-16&description=Wooden Thali&price=1800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-016.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-017.jpeg \
+  --body ~/Downloads/thali/thali-017.jpeg \
+  --tagging "name=thali-17&description=Small Wooden Thali&price=900&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-017.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-018.jpeg \
+  --body ~/Downloads/thali/thali-018.jpeg \
+  --tagging "name=thali-18&description=Steel Thali&price=2200&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-018.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-019.jpeg \
+  --body ~/Downloads/thali/thali-019.jpeg \
+  --tagging "name=thali-19&description=Steel Thali&price=2600&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-019.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-020.jpeg \
+  --body ~/Downloads/thali/thali-020.jpeg \
+  --tagging "name=thali-20&description=Steel Thali&price=2800&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-020.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/thali/thali-021.jpeg \
+  --body ~/Downloads/thali/thali-021.jpeg \
+  --tagging "name=thali-21&description=Steel Thali&price=1100&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/thali/thali-021.jpeg"
