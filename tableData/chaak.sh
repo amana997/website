d62949472da6384fcdc10ffb2e8f4201c71593c7
Aman Agrawal <aman@Amans-MacBook-Air.local>
2025-12-28 14:29:18 +0530

    CodeChef

---
+++ tableData/chaak.sh
@@ -0,0 +1,55 @@
+#commands to upload the data
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-001.jpeg \
+  --body ~/Downloads/chaak/chaak-001.jpeg \
+  --tagging "name=chaak-01&description=Chaak&price=900&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-001.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-002.jpeg \
+  --body ~/Downloads/chaak/chaak-002.jpeg \
+  --tagging "name=chaak-02&description=Chaak&price=500&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-002.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-003.jpeg \
+  --body ~/Downloads/chaak/chaak-003.jpeg \
+  --tagging "name=chaak-03&description=Chaak&price=500&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-003.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-004.jpeg \
+  --body ~/Downloads/chaak/chaak-004.jpeg \
+  --tagging "name=chaak-04&description=Chaak&price=600&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-004.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-005.jpeg \
+  --body ~/Downloads/chaak/chaak-005.jpeg \
+  --tagging "name=chaak-05&description=Chaak&price=600&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-005.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-006.jpeg \
+  --body ~/Downloads/chaak/chaak-006.jpeg \
+  --tagging "name=chaak-06&description=Chaak&price=400&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-006.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-007.jpeg \
+  --body ~/Downloads/chaak/chaak-007.jpeg \
+  --tagging "name=chaak-07&description=Chaak&price=400&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-007.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-008.jpeg \
+  --body ~/Downloads/chaak/chaak-008.jpeg \
+  --tagging "name=chaak-08&description=Chaak&price=800&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-008.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/shadiItem/chaak/chaak-009.jpeg \
+  --body ~/Downloads/chaak/chaak-009.jpeg \
+  --tagging "name=chaak-09&description=Chaak&price=500&category=chaak&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/shadiItem/chaak/chaak-009.jpeg"
---
+++ tableData/rcplatter.sh
@@ -0,0 +1,43 @@
+#commands to upload the data
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/ringceremony/platter/rcPlatter-001.jpeg \
+  --body ~/Downloads/ringCeremonyPlattaer/rcPlatter-001.jpeg \
+  --tagging "name=RC-Platter-01&description=Ring Ceremony Platter&price=2500&category=Ring Ceremony Platter&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/ringceremony/platter/rcPlatter-001.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/ringceremony/platter/rcPlatter-002.jpeg \
+  --body ~/Downloads/ringCeremonyPlattaer/rcPlatter-002.jpeg \
+  --tagging "name=RC-Platter-02&description=Ring Ceremony Platter&price=2500&category=Ring Ceremony Platter&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/ringceremony/platter/rcPlatter-002.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/ringceremony/platter/rcPlatter-003.jpeg \
+  --body ~/Downloads/ringCeremonyPlattaer/rcPlatter-003.jpeg \
+  --tagging "name=RC-Platter-03&description=Ring Ceremony Platter&price=2500&category=Ring Ceremony Platter&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/ringceremony/platter/rcPlatter-003.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/ringceremony/platter/rcPlatter-004.jpeg \
+  --body ~/Downloads/ringCeremonyPlattaer/rcPlatter-004.jpeg \
+  --tagging "name=RC-Platter-04&description=Ring Ceremony Platter&price=2500&category=Ring Ceremony Platter&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/ringceremony/platter/rcPlatter-004.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/ringceremony/platter/rcPlatter-005.jpeg \
+  --body ~/Downloads/ringCeremonyPlattaer/rcPlatter-005.jpeg \
+  --tagging "name=RC-Platter-05&description=Ring Ceremony Platter&price=2500&category=Ring Ceremony Platter&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/ringceremony/platter/rcPlatter-005.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/ringceremony/platter/rcPlatter-006.jpeg \
+  --body ~/Downloads/ringCeremonyPlattaer/rcPlatter-006.jpeg \
+  --tagging "name=RC-Platter-06&description=Ring Ceremony Platter&price=2500&category=Ring Ceremony Platter&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/ringceremony/platter/rcPlatter-006.jpeg"
+
+aws s3api put-object \
+  --bucket asha-home-products-catalog \
+  --key products/ringceremony/platter/rcPlatter-007.jpeg \
+  --body ~/Downloads/ringCeremonyPlattaer/rcPlatter-007.jpeg \
+  --tagging "name=RC-Platter-07&description=Ring Ceremony Platter&price=2500&category=Ring Ceremony Platter&image_url=https://asha-home-products-catalog.s3.ap-south-1.amazonaws.com/products/ringceremony/platter/rcPlatter-007.jpeg"
